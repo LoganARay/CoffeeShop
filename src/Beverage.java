1f8609c5d113e040b0ed137ee69d7c27530f603f
Fransisco <crazyashtro@gmail.com>
2026-04-15 13:30:34 -0400

    Beverage class

--- src/Beverage.java
+++ src/Beverage.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Beverage {
+public class Beverage {
+    private String type;        // Coffee, Latte, etc.
+    private String temperature; // Hot or Iced
+    private String size;        // Small, Medium, Large
+    private String milk;        // Whole, Skim, Almond, etc.
+    private int shots;          // 1, 2, 3 (only for espresso drinks)
+    private String sweetener;   // Sugar, Honey, Stevia, None
+
 }
