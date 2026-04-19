17721e5fc0a3a101df4c16988ba9a7946f10a859
Fransisco <crazyashtro@gmail.com>
2026-04-19 12:21:06 -0400

    BeverageFactory has been created. It's job is to create the right beverage builder.

--- src/BeverageFactory.java
+++ src/BeverageFactory.java
@@ -1,6 +1,32 @@
-public interface BeverageFactory {
-    Bevage  getSize();
-    Bevage getMilk();
-    Bevage getSweetener();
-    BeverageFactory getShots();
+
+public class BeverageFactory {
+
+    /**
+     * Factory method to get a beverage builder.
+     */
+    public static Beverage.BeverageBuilder<?> getBeverage(String type) {
+        if (type == null) {
+            return null;
+        }
+
+        // Using a switch statement to decide which concrete Builder to return
+        switch (type.toLowerCase()) {
+            case "coffee":
+                return new Coffee.Builder();
+
+            case "latte":
+                return new Latte.Builder();
+
+            case "cappuccino":
+                return new Cappuccino.Builder();
+
+            case "tea":
+                return new Tea.Builder();
+
+            default:
+                // If the type is unrecognized, we throw an error or return null.
+                System.out.println("Error: " + type + " is not on the menu.");
+                return null;
+        }
+    }
 }
